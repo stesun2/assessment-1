4ff7d248a0f3e98d5fd4363112eaa0e475c1f73b
stesun2 <ss27@illinois.edu>
2021-06-04 12:04:27 -0500

    outline basic template for optimal_change

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,12 @@
 # Write your solution here!
+
+# create Bills and Coins Dictionary
+
+def optimal_change(item_cost, amount_paid):
+
+    change = amount_paid - item_cost 
+
+    return change
+
+
+# print(optimal_change(0,0))
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,15 @@
 # Write your unit tests here!
+import unittest
+from optimal_change import optimal_change
+
+class OptimalChangeTest(unittest.TestCase):
+    """Tests for optimal_change.py"""
+
+    def test_simple_change(self):
+        self.assertEqual(optimal_change(0,0), 0)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
